e401a95d21009ab0678bfd525fe67a0258ea9218
Austin Fell <austinrf2918@yahoo.com>
2016-05-14 21:16:58 -0400

    Added stylistic changes and a scrollspy

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@ var slideIndex = 1;
 sliding = false;
 $(document).ready(function() {
 $('#fullpage').fullpage({
-
+    autoScrolling: false,
 
 });
 });
+
+$('body').scrollspy({target: '#navbar-example'});
